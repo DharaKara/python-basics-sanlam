7408aae05bc1c9471d3b45dfb33ef26f3d46539a
DharaKara <160475305+DharaKara@users.noreply.github.com>
2024-02-20 08:03:28 +0000

    changes

--- conditionals.py
+++ conditionals.py
@@ -3,9 +3,9 @@ h1= int(input('please provide height in cm'))
 p2= input('please give your name')
 h2= int(input('please provide height in cm'))
 
-if h1>h2:
+if (h1>h2):
   print(f"{p1} is taller")
-elif h1==h2:
+elif (h1==h2):
   print(f"{p1} and {p2} is of the same height")
 else:
   print(f"{p2} is taller")
