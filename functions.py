6699615a1f68c4b0132da0eabe6f3b095ae20149
DharaKara <160475305+DharaKara@users.noreply.github.com>
2024-02-22 07:26:38 +0000

    adding to functions

---
+++ functions.py
@@ -0,0 +1,29 @@
+# a=8
+# b=10
+# print("The sum is: ", a+b)
+
+# a=40
+# b=70
+# print("The sum is: ", a+b)
+
+# functions to pack knowledge - avoid logic being repeated (definition/declaration)
+def sum(a,b): # function name, # arguments/parameters
+  return a+b # body of function
+
+# abstracted logic - no need to know how it works, hiding implementation
+print(sum(30,50)) # calling function
+print(sum(50,40))
+
+# default values (argument) should come at last
+def driving_test(name,age=15,car="Toyota tazz"): # function name, # arguments/parameters
+  if age>=18:
+    return f"{name} You're eligible for driving. You will be tested with {car}."
+  else:
+    return "Try again after a few years 👶🍼"
+
+# print(driving_test(20),"GR Corrolla")
+# print(driving_test(20))
+# print(driving_test(21,"dhara")) # type error
+# print(driving_test(age=21,name="dhara")) # no error keyword arguments
+# [9,3,8].sort(reverse=true)
+# print(driving_test("dhara"))
