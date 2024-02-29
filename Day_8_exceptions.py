8cc4d15ea21d420915e2affd1c6fc43d050d9aa5
dharakara <dhara.kara@sanlam.co.za>
2024-02-29 09:34:14 +0200

    error handing exercises

--- Day_8_exceptions.py
+++ Day_8_exceptions.py
@@ -22,17 +22,48 @@ from datetime import datetime
 
 def calculate_age():
     try:
-        year = int(input("Please tell me your year of birth: "))
+        year_input = input("Please tell me your year of birth: ")
+        year = int(year_input)
         current_year = datetime.now().year
         age = current_year - year
         print(f"Your age is {age}")
-    except ValueError:
-        print("Incorrect input. Please enter a valid year.")
+    except ValueError as ve:
+        print(f"Error: {ve}")
+        print("Please enter a valid year.")
+    except TypeError as te:
+        print(f"Error: {te}")
+        print("Unexpected error occurred. Please try again.")
+    except (
+        Exception
+    ) as e:  # use only for worse case scenarios when you dont know what to expect, catches all errors
+        print(f"Unexpected error: {e}")
     finally:
         print("Done")
 
 
 calculate_age()
+
+
+# Create your own Error Class
+class NegativeNumberError(Exception):
+    def __init__(self, value):
+        self.value = value
+        self.message = "Negative numbers are not allowed"
+        super().__init__(self.message)
+
+    def __str__(self):
+        return f"{self.value} --> {self.value}"
+
+
+def only_positive_num():
+    try:
+        x = -10
+        if x < 0:
+            raise NegativeNumberError(x)
+    # match what type of error
+    except NegativeNumberError as e:
+        print(e)
+
 
 # # Task 1
 # # Please tell me your year of birth -> 2000
